e3431cd3aec995b86dbd750586645a79a621b0d2
simon-verhoeven <simonaverhoeven@gmail.com>
2021-07-09 21:12:48 +0800

    bla

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, Prop, h } from '@stencil/core';
 
 @Component({
   tag: 'uc-side-drawer',
@@ -7,14 +7,26 @@ import { Component, h, Prop } from '@stencil/core';
 })
 export class SideDrawer {
   @Prop({ reflect: true }) title: string;
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  onCloseDrawer() {
+    this.open = false;
+  }
 
   render() {
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
+          <button onClick={() => this.onCloseDrawer()}>X</button>
         </header>
-        <slot />
+        <section id='tabs'>
+          <button class='active'>Navigation</button>
+          <button>Contact</button>
+        </section>
+        <main>
+          <slot />
+        </main>
       </aside>
     );
   }
