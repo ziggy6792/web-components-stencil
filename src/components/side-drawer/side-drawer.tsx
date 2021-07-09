79b97dcdf09b0f0067ca163c2991768330c72114
simon-verhoeven <simonaverhoeven@gmail.com>
2021-07-09 20:18:18 +0800

    stuff

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,10 +1,21 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'uc-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true,
 })
 export class SideDrawer {
+  @Prop({ reflect: true }) title: string;
+
   render() {
-    return <div>hi</div>;
+    return (
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+        </header>
+        <slot />
+      </aside>
+    );
   }
 }
