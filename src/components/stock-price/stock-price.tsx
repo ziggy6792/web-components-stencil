484218470d2cc8f61adfa8de33996059f893b62c
simon-verhoeven <simonaverhoeven@gmail.com>
2021-07-13 13:16:33 +0800

    button disable

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,5 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
+import { AV_API_KEY } from '../../global/global';
 
 @Component({
   tag: 'uc-stock-price',
@@ -6,19 +7,48 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class StockPrice {
+  @State() fetchedPrice: number;
+
+  @State() sockUserInput: string;
+
+  @State() stockInputValid = false;
+
+  stockInput: HTMLInputElement;
+
   onFetchStockPrice(event: Event) {
     event.preventDefault();
-    console.log('Submitted!');
+    const stockSymbol = this.stockInput.value;
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((parsedRes) => {
+        this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
+        console.log(this.fetchedPrice);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
     return [
-      <form onSubmit={this.onFetchStockPrice}>
-        <input id='stock-symbol' />
-        <button type='submit'>Fetch</button>
+      <form onSubmit={(e) => this.onFetchStockPrice(e)}>
+        <input
+          id='stock-symbol'
+          ref={(el) => (this.stockInput = el)}
+          value={this.sockUserInput}
+          onInput={(e) => {
+            this.sockUserInput = (e.target as HTMLInputElement).value;
+            this.stockInputValid = this.sockUserInput.trim()?.length > 0;
+          }}
+        />
+        <button type='submit' disabled={!this.stockInputValid}>
+          Fetch
+        </button>
       </form>,
       <div>
-        <p>Price: {0}</p>
+        <p>Price: ${this.fetchedPrice}</p>
       </div>,
     ];
   }
---
+++ src/global/global.ts
@@ -0,0 +1,1 @@
+export const AV_API_KEY = 'Y7NOMT4I9B26BWSZ';
